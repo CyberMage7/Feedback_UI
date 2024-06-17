4b13da36bd06220f4a4b4fea2af012e1fe69119c
CyberMage7 <technician9012@gmail.com>
2024-06-17 13:43:53 +0530

    Added React.js files

---
+++ feedback-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/Header'
+import './index.css'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
