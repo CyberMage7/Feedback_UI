a9c085e37fa23e448662f7612f9cbf9ef703da5c
CyberMage7 <technician9012@gmail.com>
2024-06-17 23:35:17 +0530

    Added Card Components

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import './index.css'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
 
 export default function App() {
+
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <div>
       <Header />
+      <FeedbackList feedback={feedback}/>
     </div>
   )
 }
