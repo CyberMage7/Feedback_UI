f257e6965c0fdd471c09c760006da901d025ff5d
CyberMage7 <technician9012@gmail.com>
2024-06-18 16:29:51 +0530

    Added Delete Function

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
-import Header from './components/Header'
-import './index.css'
-import FeedbackData from './data/FeedbackData'
-import FeedbackList from './components/FeedbackList'
+import React, { useState } from "react";
+import Header from "./components/Header";
+import "./index.css";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 export default function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <div>
       <Header />
-      <FeedbackList feedback={feedback}/>
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
     </div>
-  )
+  );
 }
--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
-function FeedbackList({ feedback }) {
+import PropTypes from "prop-types";
+
+function FeedbackList({ feedback, handleDelete }) {
   if (feedback.length === 0 || !feedback) {
     return <p>No Feedback Yet</p>;
   }
@@ -8,10 +10,20 @@ function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
