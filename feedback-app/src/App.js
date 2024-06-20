8913c40c341cd3bbff6673a78a69da073ba76e39
CyberMage7 <technician9012@gmail.com>
2024-06-20 11:54:16 +0530

    Added routing

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,4 +1,5 @@
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
 import Header from "./components/Header";
 import "./index.css";
@@ -6,14 +7,15 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  const addFeedback = (newFeedback)=>{
+  const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback,...feedback]);
-  }
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
@@ -22,11 +24,27 @@ export default function App() {
   };
 
   return (
-    <div>
+    <Router>
       <Header />
-      <FeedbackForm handleAdd={addFeedback} />
-      <FeedbackStats feedback={feedback} />
-      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-    </div>
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" component={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
---
+++ feedback-app/src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../components/shared/Card";
+
+export default function AboutPage() {
+  return (
+    <Card>
+      <div className="about-link">
+        <h1>About this project</h1>
+        <p>This a react app t0 leave feedback for a product or service</p>
+      </div>
+    </Card>
+  );
+}
