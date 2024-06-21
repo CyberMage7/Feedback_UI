997b50cad356f84dafedd794d45b2b714b84a7ea
CyberMage7 <technician9012@gmail.com>
2024-06-21 12:37:25 +0530

    Connected FrontEnd and Backend

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,19 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This item is from context",
-      rating: 10,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(()=>{
+    fetchFeedback()
+  }, []);
+
+  const fetchFeedback = async()=>{
+    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  }
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -43,6 +49,7 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
